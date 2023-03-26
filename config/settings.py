8aaab9c33c5d4e30947da3d275d6214d10d50361
백관열 <djm030@naver.com>
2023-03-26 21:23:28 +0900

    change database

--- config/settings.py
+++ config/settings.py
@@ -107,8 +107,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "djangoDatabase",
+        "USER": "djm030",
+        "PASSWORD": "djm062954!",
+        "HOST": "djangosql-001.db-fnkpj.cdb.ntruss.com",
+        "PORT": "3306",
     }
 }
 
